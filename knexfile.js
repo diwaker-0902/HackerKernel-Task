c66b186acbe16f38b1f2d052759d5853f92fcf44
Diwaker Singh <115572224+diwaker-0902@users.noreply.github.com>
2025-06-29 19:11:40 +0530

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -5,8 +5,8 @@ module.exports = {
     client: "mysql2",
     connection: {
       host: process.env.DB_HOST || "localhost",
-      user: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "root",
+      user: process.env.DB_USER || "your-db-name",
+      password: process.env.DB_PASSWORD || "your-db-name",
       database: process.env.DB_NAME || "node_task_app",
       // database: process.env.DB_NAME || "hacker_kernel",
     },
